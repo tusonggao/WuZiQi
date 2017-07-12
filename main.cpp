3dbe4cc80a5b0f791aa5ee5492103932dbbd979a
Song-gao Tu <tusonggao@163.com>
2017-07-12 21:49:39 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 
 // I am doing this to remind myself to code everyday!
+// I am doing this to remind myself to code everyday!
+// I am doing this to remind myself to code everyday!
 
 int main(int argc, char *argv[])
 {
