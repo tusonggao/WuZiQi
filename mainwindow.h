2f3eb01ee54347fb4e5c1148aba958ea6a1fd75b
Song-gao Tu <tusonggao@163.com>
2017-07-26 22:39:53 +0800

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -5,7 +5,7 @@
 #include "wuziplayboarddesktop.h"
 
 //I am doing this to remind myself to code EVERYDAY! Today, I have written code on my local computer.
-#I am doing this to remind myself to code EVERYDAY! Today, I have written code on my local computer.
+//I am doing this to remind myself to code EVERYDAY! Today, I have written code on my local computer.
 
 
 
