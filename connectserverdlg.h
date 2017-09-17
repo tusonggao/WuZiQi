567b90eec9b902bf7f1fd760cb4870a46f38e823
Song-gao Tu <tusonggao@163.com>
2017-09-17 22:46:56 +0800

    Update connectserverdlg.h

--- connectserverdlg.h
+++ connectserverdlg.h
@@ -8,7 +8,7 @@
 // I am doing this to remind myself to program everyday.
 // I am doing this to remind myself to program everyday.
 // I am doing this to remind myself to program everyday.
-// I am doing this to remind myself to program everyday.
+
 
 
 
